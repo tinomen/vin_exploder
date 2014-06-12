7a56bd0874969ae9846f2da7c1f4d1b974063fb1
Tad Thorley <phaedryx@gmail.com>
2014-06-12 16:56:50 -0600

    Exploder can take multiple adapters and updated Exploder tests

--- lib/vin_exploder/exploder.rb
+++ lib/vin_exploder/exploder.rb
@@ -1,31 +1,34 @@
 module VinExploder
-  
-  class MissingAdapter < StandardError
-  end
-  
+  class MissingAdapter < StandardError; end
+
   class Exploder
-    
-    def initialize(adapter, cache=nil)
-      raise MissingAdapter.new("No vin decoding vendor adapter has been provided.") if adapter.nil?
-      @cache = cache ? cache : Cache::Store.new
-      @adapter = adapter
+
+    def initialize(adapters, cache=nil)
+      raise ArgumentError.new("adapters must be an array") unless adapters.kind_of? Array
+      raise MissingAdapter.new("No vin decoding vendor adapter has been provided.") if !adapters || adapters.empty?
+      @cache    = cache ? cache : Cache::Store.new
+      @adapters = adapters
     end
-    
+
     # Get vin Explosion.
     #
     # == Parameters
-    # vin:: vehicle identification number 
+    # vin:: vehicle identification number
     #
     # == Return
     # An Explosion object containing the decoded vin attributes
     def get(vin)
       hash = @cache.fetch(vin) do
-        @adapter.explode(vin)
+        # find first adapter that doesn't 'pass' on the vin
+        @adapters.each do |a|
+          results = a.explode(vin)
+          break results unless results[:pass]
+        end
       end
       errors = hash ? hash.delete(:errors) : []
       data = hash ? hash : {}
       Explosion.new vin, data, errors
     end
-    
+
   end
 end
--- spec/vin_exploder/exploder_spec.rb
+++ spec/vin_exploder/exploder_spec.rb
@@ -4,31 +4,91 @@ require 'vin_exploder/exploder'
 module VinExploder
 
   describe Exploder do
-    describe '#new' do
-      it "should raise a MissingAdapter error if nil is provided to initialize" do
-        expect { Exploder.new(nil) }.to raise_error(MissingAdapter)
+    class Adapter
+      def initialize(vin_hash={})
+        @vin_hash = vin_hash
       end
+      def explode(vin)
+        @vin_hash
+      end
+    end
 
+    class PassAdapter
+      def explode(vin)
+        {pass: "someone else should"}
+      end
+    end
+
+    class SimpleCache < Cache::Store
+      def read(vin)
+        @cache ||= {}
+        @cache[vin]
+      end
+      def write(vin, hash)
+        @cache ||= {}
+        @cache[vin] = hash
+      end
+    end
+
+    describe '#new' do
       it "should raise a ArgumentError error if no arguments are provided to initialize" do
         expect { Exploder.new }.to raise_error(ArgumentError)
       end
 
+      it "should raise a ArgumentError error if nil is provided to initialize" do
+        expect { Exploder.new(nil) }.to raise_error(ArgumentError)
+      end
+
+      it "should raise a MissingAdapter error if and empty array is provided to initialize" do
+        expect { Exploder.new([]) }.to raise_error(MissingAdapter)
+      end
+
       it "should initialize with a default cache store when no cache argument is passed" do
-        s = VinExploder::Cache::Store.new
-        VinExploder::Cache::Store.should_receive(:new).exactly(1).times { s }
-        e = Exploder.new(double("Adapter"))
+        store = VinExploder::Cache::Store.new
+        adapter = Adapter.new
+        VinExploder::Cache::Store.should_receive(:new).exactly(1).times { store }
+        exploder = Exploder.new([adapter])
       end
     end
 
     describe '#get' do
       it "should get a vin Explosion" do
-        a = double("Adapter")
-        a.should_receive(:explode) { {} }
-        e = Exploder.new(a)
-        ex = e.get('VIN')
-        ex.class.should == VinExploder::Explosion
-        ex.valid?.should == true
-        ex.success?.should == true
+        adapter   = Adapter.new
+        exploder  = Exploder.new([adapter])
+        explosion = exploder.get('VIN')
+        explosion.class.should == VinExploder::Explosion
+        explosion.valid?.should == true
+        explosion.success?.should == true
+      end
+
+      it "should return the cached vin explosion if it is cached" do
+        cache     = SimpleCache.new
+        cache.write('VIN', {make: 'Ford', model: 'F150'})
+        adapter   = Adapter.new
+        exploder  = Exploder.new([adapter], cache)
+        explosion = exploder.get('VIN')
+
+        explosion.make.should == 'Ford'
+      end
+
+      it "should return the adapter vin explosion if it isn't cached" do
+        cache     = SimpleCache.new
+        adapter   = Adapter.new(make: 'Dodge')
+        exploder  = Exploder.new([adapter], cache)
+        explosion = exploder.get('VIN')
+
+        explosion.make.should == 'Dodge'
+      end
+
+      it "should return results from the first adapter that doesn't pass" do
+        cache     = SimpleCache.new
+        p1adapter = PassAdapter.new
+        p2adapter = PassAdapter.new
+        adapter   = Adapter.new(make: 'Subaru', model: 'Impreza')
+        exploder  = Exploder.new([p1adapter, p2adapter, adapter])
+        explosion = exploder.get('VIN')
+
+        explosion.model.should == 'Impreza'
       end
     end
   end
