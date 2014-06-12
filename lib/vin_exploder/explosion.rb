e7fcebcfa2c84a405816174a38afc05905a66a7c
Tad Thorley <phaedryx@gmail.com>
2014-06-12 17:02:30 -0600

    my auto formatting strikes again

--- lib/vin_exploder/explosion.rb
+++ lib/vin_exploder/explosion.rb
@@ -1,31 +1,30 @@
 require 'yaml'
 
 module VinExploder
-  
+
   class Explosion
-    
+
     attr_reader :vin_key, :success, :errors, :make, :model, :year, :driveline, :body_style, :fuel_type, :number_of_doors, :all
-    
+
     def initialize(vin, vin_hash, errors=[])
-      @vin = vin
-      @all = vin_hash
-      @make = @all[:make]
-      @model = @all[:model]
-      @year = @all[:year]
-      @driveline = @all[:driveline]
-      @body_style = @all[:body_style]
-      @fuel_type = @all[:fuel_type]
+      @vin             = vin
+      @all             = vin_hash
+      @make            = @all[:make]
+      @model           = @all[:model]
+      @year            = @all[:year]
+      @driveline       = @all[:driveline]
+      @body_style      = @all[:body_style]
+      @fuel_type       = @all[:fuel_type]
       @number_of_doors = @all[:number_of_doors]
-      @trim_level = @all[:trim_level]
-      @errors = errors.nil? ? [] : errors
-      @success = @errors.empty?
+      @trim_level      = @all[:trim_level]
+      @errors          = errors.nil? ? [] : errors
+      @success         = @errors.empty?
     end
-    
+
     def valid?
       @success
     end
     alias :success? :valid?
-    
   end
-  
+
 end
