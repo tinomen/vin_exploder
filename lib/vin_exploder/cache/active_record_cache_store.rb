2b252d4fe329d022cab3f69003d825378b7f6ba4
Tad Thorley <phaedryx@gmail.com>
2014-06-12 17:05:25 -0600

    clarify code for myself a bit

--- lib/vin_exploder/cache/active_record_cache_store.rb
+++ lib/vin_exploder/cache/active_record_cache_store.rb
@@ -1,42 +1,39 @@
 require 'active_record'
 require 'base64'
 
-module VinExploder
-module Cache
-  
+module VinExploder::Cache
   # A VinExploder cache adapter using ActiveRecord for saving the decoded vin attributes.
   #
   # this store assumes a model with 2 attributes:
   # - *key*: the first 8, 10th and 11th characters of the vin
   # - *data*: A hash of the decoded vin attributes
   class ActiveRecordCacheStore < Store
-    
+
     def initialize(options = {})
       super
       @model_class = options[:model_class]
     end
-    
+
     def read(vin)
       key = make_vin_cache_key(vin)
       obj = @model_class.find_by_key(key)
       obj.data unless obj.nil?
     end
-    
+
     def write(vin, hash)
       key = make_vin_cache_key(vin)
       obj = @model_class.new :key => key, :data => hash
       obj.save
       hash
     end
-    
+
     def delete(vin)
       key = make_vin_cache_key(vin)
       obj = @model_class.find_by_key(key)
       deleted = obj.delete()
       !deleted.nil?
     end
-    
+
   end
-  
-end
+
 end
--- lib/vin_exploder/cache/couchrest_cache_store.rb
+++ lib/vin_exploder/cache/couchrest_cache_store.rb
@@ -1,41 +1,39 @@
 require 'couchrest'
 
-module VinExploder
-module Cache
-  
+module VinExploder::Cache
+
   # A VinExploder cache adapter using CouchDB for saving the decoded vin attributes.
   #
   # this store assumes there is 1 attribute in the document:
   # - *data*: A hash of the decoded vin attributes
   class CouchrestCacheStore < Store
-    
+
     def initialize(options = {})
       super
       srv = CouchRest.new options[:host]
       @db = srv.database!(options[:db_name])
       # vin_view = db.save_doc({"_id" => "_design/vins", :views => {:vins => {:map => 'function(doc){ if(doc.key){ emit(doc.key, doc.data) } }'}}}) unless db.get("_design/vins")
     end
-    
+
     def read(vin)
       key = make_vin_cache_key(vin)
       result = @db.get(key)['data'] rescue nil
       hash = symbolize_result_hash(result) unless result.nil?
       hash
     end
-    
+
     def write(vin, hash)
       key = make_vin_cache_key(vin)
       @db.save_doc({"_id" => key, :data => hash})
       hash
     end
-    
+
     def delete(vin)
       key = make_vin_cache_key(vin)
       result = @db.delete_doc(@db.get(key))
       !result.nil? && result['ok']
     end
-    
+
   end
-  
-end
+
 end
